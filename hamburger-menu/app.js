4f819f599378b522ec9027da9d842bf1e267e511
Metlokitok <kylebaltazar.161@gmail.com>
2025-03-20 22:07:24 +0800

    Modified the css styling so that it starts at the mobile form

--- hamburger-menu/app.js
+++ hamburger-menu/app.js
@@ -4,7 +4,8 @@ const navMenu = document.querySelector("#nav-bar")
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active");
-
+    document.body.classList.toggle("no-scroll");
+    
     const icon = hamburger.querySelector("i")
     icon.classList.toggle("fa-bars");
     icon.classList.toggle("fa-times");
