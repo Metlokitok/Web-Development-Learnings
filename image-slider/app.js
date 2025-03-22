dd8016e67175264c3b35c6f3c876a4912396ecb7
Metlokitok <kylebaltazar.161@gmail.com>
2025-03-22 22:43:21 +0800

    Modified the slider, added button indicators

--- image-slider/app.js
+++ image-slider/app.js
@@ -1,8 +1,33 @@
 const imgSlider = document.querySelector(".slider-img");
 const images = document.querySelectorAll(".slider-img img");
+const dots = document.querySelectorAll(".dot")
 const prevBtn = document.getElementById("btn-prev");
 const nextBtn = document.getElementById("btn-next");
 let currentIndex = 0;
+
+// function for dots
+function activeDot() {
+    for (let i=0;i<dots.length;i++) {
+        if (i===currentIndex) {
+            dots[i].classList.add("active")
+        } else {
+            dots[i].classList.remove("active")
+        }
+    }
+}
+
+// Makes the dots interactive
+for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener("click", () => {
+        currentIndex = i;
+        imgSlider.style.transform = `translateX(${-currentIndex * 60}rem)`;
+        activeDot();
+    });
+}
+
+// Ensure that the first dot has the active class when the page loads
+dots[currentIndex].classList.add("active") 
+
 
 // Button controls for selecting previous image
 prevBtn.addEventListener("click", () => {
@@ -12,6 +37,7 @@ prevBtn.addEventListener("click", () => {
     }
     console.log(`Current Index: ${currentIndex}`);
     imgSlider.style.transform = `translateX(${-currentIndex * 60}rem)`
+    activeDot()
 });
 
 // Button controls for selecting next image
@@ -22,6 +48,7 @@ nextBtn.addEventListener("click", () => {
     }
     console.log(`Current Index: ${currentIndex}`);
     imgSlider.style.transform = `translateX(${-currentIndex * 60}rem)`
+    activeDot()
 });
 
 // Sets the automatic slider
@@ -29,8 +56,7 @@ setInterval(()=>{
     currentIndex++;
     if (currentIndex > images.length - 1) {
         currentIndex = 0;
-    } else if (currentIndex < 0) {
-        currentIndex = images.length - 1;
-    }
+    } 
     imgSlider.style.transform = `translateX(${-currentIndex * 60}rem)`
+    activeDot()
 },2000);
